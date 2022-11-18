8616ec7bd1072f04b7ed323d6af102e713aa583e
Danykrane <gvg5555@gmail.com>
2022-11-18 21:47:03 +0300

    upt

---
+++ 1 less/Практика/1_002.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+ float wes;
+ __uint16_t rost, razm;
+ cout << "Введите информацию"<<endl;
+ cin >> wes >> rost >> razm;
+
+ cout << "Вес: "<< wes<<endl;
+ cout <<"Рост: "<<rost<<endl;
+ cout <<"Разм битс: "<<razm<<endl;
+
+
+return 0;
+
+
+}
---
+++ 1 less/Практика/firstr_prog.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+int age;
+cout << "Введите возраст"<<endl;
+cin >> age;
+
+
+cout << "Ваш возраст "<<age<<endl;
+
+return 0;
+
+
+}
