44a4d5efd73bf24eb2ddc7af363c69f869801443
Danykrane <gvg5555@gmail.com>
2022-11-18 15:06:38 +0300

    1 less

--- 1 less/Практика/1_02.cpp
+++ 1 less/Практика/1_02.cpp
@@ -2,11 +2,10 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
 
-int number1 = 45123;
-// 4 2 
-cout << number1 % 2<<endl;//4
-
-
+    int number1 = 45123;
+    // 4 2
+    cout << number1 % 2 << endl; // 1
 }
