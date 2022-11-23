8d36e839988a71bdbfc6fc8d283ead2b39fc7b59
Danykrane <gvg5555@gmail.com>
2022-11-23 12:58:41 +0300

    add 2 less

---
+++ 1 less/Практика/temp.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+#include <cmath>
+#include <iomanip>
+int main()
+{
+  short a,b;
+  int s;
+    cout << "Введите 2 числа"<< endl;
+    cin >> a>> b;
+  unsigned char operation; 
+    cout << "Какую операцию хотите сделать?"<< endl;
+    cout << "1 - сложение"<< endl;
+    cout << "2 - вычитание"<< endl;
+    cout << "3 - умножение"<< endl;
+    cout << "4 - деление"<< endl;
+    cout << "5 - возведение в степень "<< endl;
+    cout << "6 - логарифм по основанию a числа b"<< endl;
+    
+    cin >> operation;
+
+    if (operation=='1') {
+      
+        cout << a+b << endl;
+    }
+    else if (operation == '2') {
+        cout << a-b;
+    }
+    else if (operation == '3') {
+        cout << a*b;
+    }
+    else if (operation == '4') {
+        cout << (a*1.0)/b;
+    }
+    else if (operation == '5') {
+        cout << pow(a,b);
+    }
+    else if (operation == '6') {
+        cout << log(b*1.0)/log(a*1.0);
+    }
+    else {
+        cout << "Вы неправильно указали номер"; }
+   cout <<fixed << setprecision(5)<<endl;
+}
