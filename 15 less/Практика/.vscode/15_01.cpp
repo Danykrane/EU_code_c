304f80464f36b32ea25845a9c163765d0e82f718
Danykrane <gvg5555@gmail.com>
2023-01-20 19:32:34 +0300

    add 15 less

---
+++ 15 less/Практика/.vscode/15_01.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Animal
+{
+
+    string name;
+    int vozr;
+
+    void valid()
+    {
+        if (vozr < 0)
+            vozr = 0;
+    }
+
+public:
+    void setAll(string test, int age)
+    {
+        name = test;
+        vozr = age;
+        valid();
+    }
+
+    string getN()
+    {
+        return name;
+    }
+
+    int getA()
+    {
+        return vozr;
+    }
+};
+// - -> 0
+// 20 > -> 20
+
+void read(Animal &cat)
+{
+    string name;
+    cin >> name;
+    cat.setN(name);
+    int age;
+    cin >> age;
+    cat.setA(age);
+    // validate(cat);
+}
+
+// void validate(Animal &cat)
+
+void show(Animal &cat)
+{
+    cout << "Информация о животном: ";
+    cout << cat.getN() << " " << cat.getA();
+}
+
+int main()
+{
+    Animal cot;
+    // cot.setN(name);
+    // cot.setA(24);
+    read(cot);
+    show(cot);
+    // cout << cot.getN() << " "<<cot.getA();
+}
